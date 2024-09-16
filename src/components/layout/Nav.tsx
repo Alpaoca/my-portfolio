06f1dfaf23a2bcec711bfea9785812b153ea060d
Alpaoca <preechak10123@gmail.com>
2024-09-17 01:40:21 +0700

    add burger icon

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+
 function Nav() {
+  const [toggle, setToggle] = useState(false);
+  const toggleState = () => {
+    setToggle((prevState) => !prevState);
+  };
+
   return (
     <>
-      <nav>
-        <p>Resume</p>
-        <p>Skill</p>
-        <p>Contact</p>
+      <nav className="absolute left-[1.5rem] top-[1.5rem] w-[10rem]">
+        <button onClick={toggleState} className="rounded-full p-[1rem]">
+          <img
+            src="images\burger-menu-svgrepo-com.svg"
+            className="size-[2rem]"
+            alt="Navigation burger menu icon"
+          />
+        </button>
+        {toggle && (
+          <>
+            <p>Resume</p>
+            <p>Skill</p>
+            <p>Contact</p>
+          </>
+        )}
       </nav>
     </>
   );
