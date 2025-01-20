9706a94a4c6f8a4edc35e81142b6a2dcb8595add
Alpaoca <preechak10123@gmail.com>
2025-01-20 14:17:56 +0700

    Updates

--- src/components/shared/BoxComponent.tsx
+++
@@ -1,9 +0,0 @@
-function BoxComponent({ text }: any) {
-  return (
-    <div className="bg-[#f7f7f7] border border-transparent inline-flex items-center rounded-md text-xs font-semibold font-sans text-pretty px-2 py-0 md:text-sm xl:text-nm">
-      {text}
-    </div>
-  );
-}
-
-export default BoxComponent;
